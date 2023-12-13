22424587e4992624a7f607ce18563827702dec5a
Nikita Lomeiko <lomieiko78@mail.ru>
2023-12-13 09:50:51 +0300

    init

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '127.0.0.1',
+    port: 8000
+  },
+  resolve:{
+    alias: {
+      app: '/src/app',
+      pages: '/src/pages',
+      widgets: '/src/widgets',
+      features: '/src/features',
+      entities: '/src/entities',
+      shared: '/src/shared'
+    }
+  }
+})
